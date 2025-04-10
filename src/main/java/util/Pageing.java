e46b20023e918409be1ddb84915a0fe71df68b1e
youjin119 <youjin3012@naver.com>
2025-04-10 09:48:16 +0900

    페이징 수정v1.1

--- src/main/java/util/Pageing.java
+++ src/main/java/util/Pageing.java
@@ -5,10 +5,10 @@ public class Pageing {
             int pageNum, String reqUrl, String tag) { // tag 매개변수 추가
         String pagingStr = "";
 
-        // 단계 3 : 전체 페이지 수 계산
+        //  전체 페이지 수 계산
         int totalPages = (int) (Math.ceil(((double) totalCount / pageSize)));
 
-        // 단계 4 : '이전 페이지 블록 바로가기' 출력
+        //  '이전 페이지 블록 바로가기' 출력
         int pageTemp = (((pageNum - 1) / blockPage) * blockPage) + 1;
         if (pageTemp != 1) {
             pagingStr += "<a href='" + reqUrl + "?pageNum=1&tag=" + tag + "'>[첫 페이지]</a>"; // tag 정보 추가
@@ -16,7 +16,7 @@ public class Pageing {
             pagingStr += "<a href='" + reqUrl + "?pageNum=" + (pageTemp - 1) + "&tag=" + tag + "'>[이전 블록]</a>"; // tag 정보 추가
         }
 
-        // 단계 5 : 각 페이지 번호 출력
+        // : 각 페이지 번호 출력
         int blockCount = 1;
         while (blockCount <= blockPage && pageTemp <= totalPages) {
             if (pageTemp == pageNum) {
@@ -29,7 +29,7 @@ public class Pageing {
             blockCount++;
         }
 
-        // 단계 6 : '다음 페이지 블록 바로가기' 출력
+        //  '다음 페이지 블록 바로가기' 출력
         if (pageTemp <= totalPages) {
             pagingStr += "<a href='" + reqUrl + "?pageNum=" + pageTemp + "&tag=" + tag + "'>[다음 블록]</a>"; // tag 정보 추가
             pagingStr += "&nbsp;";
